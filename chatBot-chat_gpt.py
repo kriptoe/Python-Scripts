c6112fc3152fae586bbc89f090decbd2c497794e
kriptoe <56439317+kriptoe@users.noreply.github.com>
2024-04-29 08:52:19 +0800

    Create chatBot-chat_gpt.py
    Interact with chat gpt through a python script

---
+++ chatBot-chat_gpt.py
@@ -0,0 +1,21 @@
+import openai
+# youtube tutorial https://www.youtube.com/watch?v=pGOyw_M1mNE
+
+# put your API key here
+# url to get API key https://platform.openai.com/api-keys
+openai.api_key = "#####"
+
+messages = []
+system_msg = input("What type of chatbot would you like to create?\n")
+messages.append({"role": "system", "content": system_msg})
+
+print("Your new assistant is ready!")
+while input != "quit()":
+    message = input()
+    messages.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages)
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
+    print("\n" + reply + "\n")
